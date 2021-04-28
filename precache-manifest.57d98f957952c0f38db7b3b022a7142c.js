82ca511186af5fb8324bbcbe915af2867e3dc334
Ruslan <ruslan.astratov@yandex.ru>
2021-04-28 20:34:52 +0200

    Updates

---
+++ precache-manifest.57d98f957952c0f38db7b3b022a7142c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88fd95720d5f6fd9fbc30863bf22958d",
+    "url": "/must-see/index.html"
+  },
+  {
+    "revision": "0ec8f8bb604026d84861",
+    "url": "/must-see/static/css/main.cb385a34.chunk.css"
+  },
+  {
+    "revision": "d97e6b15434da4b808de",
+    "url": "/must-see/static/js/2.8c8cad38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/must-see/static/js/2.8c8cad38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ec8f8bb604026d84861",
+    "url": "/must-see/static/js/main.a21cfb55.chunk.js"
+  },
+  {
+    "revision": "852137381cf513fc9883",
+    "url": "/must-see/static/js/runtime-main.9c1efa88.js"
+  }
+]);
